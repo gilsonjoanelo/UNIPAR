34476584944b9c4c7afbf952466c08058c3a180c
Gilson Joanelo <gilsonjoanelo@gmail.com>
2021-03-03 13:29:52 -0300

    ar

--- AR_Programacao_Internet/includes/rodape.inc.php
+++
@@ -1,9 +0,0 @@
-
-<script
-  src="../scripts/jquery-3.5.1.min.js"></script>
-<script
-  src="../scripts/popper.min.js"></script>
-<script
-  src="../scripts/bootstrap.min.js"></script>
-</body>
-</html>
